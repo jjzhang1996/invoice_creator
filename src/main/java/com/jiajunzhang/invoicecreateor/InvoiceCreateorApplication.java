600e0b227183be926492d5129f621d7d04dc9682
Jia Jun Zhang <foodmapaustria@gmail.com>
2022-08-14 22:57:26 +0200

    Init new Project from spring init.

---
+++ src/main/java/com/jiajunzhang/invoicecreateor/InvoiceCreateorApplication.java
@@ -0,0 +1,13 @@
+package com.jiajunzhang.invoicecreateor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceCreateorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceCreateorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiajunzhang/invoicecreateor/InvoiceCreateorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiajunzhang.invoicecreateor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceCreateorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
